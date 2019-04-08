f1ac2607c32df4cfec23156741deef3030282a92
Jordan Love <jnlove314@gmail.com>
2019-04-08 14:19:13 -0500

    Add Firebase initializations

--- app/static/main.js
+++ app/static/main.js
@@ -24,3 +24,12 @@ function logOut()
          window.location.href = "index.html";
     }
 }
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: '### FIREBASE API KEY ###',
+  authDomain: '### FIREBASE AUTH DOMAIN ###',
+  projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+});
+
+var db = firebase.firestore();
