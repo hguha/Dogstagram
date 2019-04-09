491cfe6654feaf0b9916b4c1d3dda9bd568e0c42
John Quitno <john.quitno@ku.edu>
2019-04-09 12:31:44 -0500

    Added login functionality

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, Response, redirect, session, 
 import os.path
 import json
 import hashlib
+from UserInfo import AddUser
+from UserInfo import CheckCredentials
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -24,9 +27,11 @@ def login():
         print(hashed_password)
 
         ##this if statement should check user and hashed_password against db
-        if ((d['username'] == 'user')): 
+        if (CheckCredentials(d['username'],hashed_password)): 
             ##starts session with this user
             session['user'] = d['username']
+        else:
+            return ("False")
     ##returns template if its not a POST
     return render_template('index.html')
 
@@ -42,11 +47,11 @@ def signup():
         h = hashlib.md5(d['password'].encode())
         hashed_password = h.hexdigest()
         print(hashed_password)
-
-        ## need to add user and hashed pass to db here
-
+        if AddUser(d['username'],hashed_password):
         ##starts session with this user
-        session['user'] = d['username']
+            session['user'] = d['username']
+        else:
+            return "False"
 
     return render_template('index.html')
 
@@ -68,5 +73,21 @@ def dropsession():
     session.pop('user', None)
     return redirect(url_for('login'))
 
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
+
