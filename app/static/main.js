525d8475276f5cb4cce17658617b476916f11423
Jordan Love <jnlove314@gmail.com>
2019-04-08 23:25:24 -0500

    resolve merge conflicts, change initialization of firebase

--- app/static/main.js
+++ app/static/main.js
@@ -1,8 +1,57 @@
-function login() {
-    //validate it or whatever I'm confused by this
-    var username = document.getElementById("username");
-    var password = document.getElementById("password");
+//initialize Firebase
+var config = {
+  apiKey: "AIzaSyAI5CEVqPwnDM7p6WmDIyQLx2V6QdfL2Nc",
+  authDomain: "dogstagram-b019f.firebaseapp.com",
+  databaseURL: "https://dogstagram-b019f.firebaseio.com",
+  projectId: "dogstagram-b019f",
+  storageBucket: "dogstagram-b019f.appspot.com",
+  messagingSenderId: "495415462838"
+};
+firebase.initializeApp(config);
 
+//set up document reference
+var firestore = firebase.firestore();
+const docRef = firestore.collection("users")
+
+
+
+function login() {
+  //get user and password from front end
+  const username = document.querySelector("#username");
+  const userInput = username.value;
+  const password = document.querySelector("#password");
+  const passInput = password.value;
+
+  let userValid = false;
+  let passvalid = false;
+
+/* currently broken
+  //query for matching username and password in Firebase
+  docRef.where("user", "==", userInput).get().then(function (doc) {
+    if (doc && doc.exists) {
+      let myData = doc.data();
+      console.log( myData );
+      if (myData.user == userInput) {
+        //userValid = true;
+        console.log( "myData.user: ");
+      }
+      if (myData.pass == passInput) {
+        //passValid = true;
+        console.log( "myData.pass: ");
+      }
+    }
+  })
+
+  console.log(password.value)
+  if ((password.value == "password") && (username.value == "username"))
+  {
+     window.location.href = "landing.html";
+  }
+  else
+  {
+      alert("The username or password you entered doesn't exist!");
+  }
+*/
     $.ajax({
         type: "POST",
         url: '/login',
@@ -14,9 +63,10 @@ function login() {
             //goes to landing after login but will return to login if user does not exist
             window.location.href = '/landing'
         }
-    })    
+    })
     console.log(password.value)
 }
+
 
 
 function signup()
@@ -33,23 +83,7 @@ function signup()
             //goes to landing after signup
             window.location.href = '/landing'
         }
-    })  
-
-    
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAI5CEVqPwnDM7p6WmDIyQLx2V6QdfL2Nc",
-    authDomain: "dogstagram-b019f.firebaseapp.com",
-    databaseURL: "https://dogstagram-b019f.firebaseio.com",
-    projectId: "dogstagram-b019f",
-    storageBucket: "dogstagram-b019f.appspot.com",
-    messagingSenderId: "495415462838"
-  };
-  firebase.initializeApp(config);
-
-  //set up document reference
-  var firestore = firebase.firestore();
-  const docRef = firestore.collection("users").doc("user1")
+    })
 
   //get user and password from front end
   const username = document.querySelector("#username");
@@ -57,14 +91,19 @@ function signup()
   const password = document.querySelector("#password");
   const passInput = password.value;
 
-  //pass user inputs to Firebase
-  console.log("I'm saving " + userInput + ' ' + passInput + " to Firestore.");
-  docRef.set
-  ({
-    user: userInput,
-    pass: passInput
+  // Add a new document with a generated id.
+  firestore.collection("users").add({
+      user: userInput,
+      pass: passInput
+  })
+  .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
   });
 }
+
 
 
 function logOut()
