36943e0ae42be9f31eb7663ae3158baafd2ed4fd
Jordan Love <jnlove314@gmail.com>
2019-04-08 20:08:24 -0500

    Sign up stores user/pass in Firebase

--- app/static/main.js
+++ app/static/main.js
@@ -12,24 +12,45 @@ function login() {
       alert("The username or password you entered doesn't exist!");
   }
 }
+
+
 function signup()
 {
-    //figure out how to get this into a database I guess?
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAI5CEVqPwnDM7p6WmDIyQLx2V6QdfL2Nc",
+    authDomain: "dogstagram-b019f.firebaseapp.com",
+    databaseURL: "https://dogstagram-b019f.firebaseio.com",
+    projectId: "dogstagram-b019f",
+    storageBucket: "dogstagram-b019f.appspot.com",
+    messagingSenderId: "495415462838"
+  };
+  firebase.initializeApp(config);
+
+  //set up document reference
+  var firestore = firebase.firestore();
+  const docRef = firestore.collection("users").doc("user1")
+
+  //get user and password from front end
+  const username = document.querySelector("#username");
+  const userInput = username.value;
+  const password = document.querySelector("#password");
+  const passInput = password.value;
+
+  //pass user inputs to Firebase
+  console.log("I'm saving " + userInput + ' ' + passInput + " to Firestore.");
+  docRef.set
+  ({
+    user: userInput,
+    pass: passInput
+  });
 }
+
 
 function logOut()
 {
-    if(confirm("are you sure you want to log out?"))
-    {
-         window.location.href = "index.html";
-    }
+  if(confirm("are you sure you want to log out?"))
+  {
+       window.location.href = "index.html";
+  }
 }
-
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-  apiKey: '### FIREBASE API KEY ###',
-  authDomain: '### FIREBASE AUTH DOMAIN ###',
-  projectId: '### CLOUD FIRESTORE PROJECT ID ###'
-});
-
-var db = firebase.firestore();
